c7c157c46b096c581bfbaeb1dab0094d96eb905e
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-05-14 01:11:12 +0300

    Add Ninject

---
+++ Coursework/DependencyResolution/RepositoryModule.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using DAL.Entity;
+using DAL.Interface;
+using DAL.Repository;
+using Ninject.Modules;
+
+namespace DependencyResolution
+{
+    public class RepositoryModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepository<Resort>>().To<ResortRepository>();
+            Bind<IRepository<Tour>>().To<TourRepository>();
+            Bind<IRepository<TourVariant>>().To<TourVariantRepository>();
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+            Bind<TourContext>().ToSelf();
+        }
+    }
+}
---
+++ Coursework/DependencyResolution/ServiceModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyResolution
+{
+    public class ServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
