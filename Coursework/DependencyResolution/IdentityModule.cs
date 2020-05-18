6bc9d11d9a02e6135a7b4a01beb52a979c240bb9
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-05-18 14:23:37 +0300

    Add Identity module. Bind services

---
+++ Coursework/DependencyResolution/IdentityModule.cs
@@ -0,0 +1,21 @@
+﻿using Ninject.Modules;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using DAL.Identity;
+using DAL.EF;
+using Ninject;
+
+namespace DependencyResolution
+{
+    public class IdentityModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<RoleManager<IdentityRole>>().To<ApplicationRoleManager>();
+            Bind<UserManager<ApplicationUser>>().To<ApplicationUserManager>();
+
+            Bind<RoleStore<IdentityRole>>().ToSelf().WithConstructorArgument("context", context => KernelInstance.Get<ApplicationDbContext>());
+            Bind<UserStore<ApplicationUser>>().ToSelf().WithConstructorArgument("context", context => KernelInstance.Get<ApplicationDbContext>());
+        }
+    }
+}
--- Coursework/DependencyResolution/RepositoryModule.cs
+++ Coursework/DependencyResolution/RepositoryModule.cs
@@ -2,9 +2,9 @@
 using DAL.EF;
 using DAL.Entity;
 using DAL.Interface;
-using DAL.Migrations.Factory;
 using DAL.Repository;
 using Ninject.Modules;
+using System.Data.Entity;
 
 namespace DependencyResolution
 {
@@ -17,7 +17,7 @@ namespace DependencyResolution
             Bind<IRepository<TourVariant>>().To<TourVariantRepository>();
             Bind<IUnitOfWork>().To<UnitOfWork>();
             
-            Bind<TourContext>().ToSelf().WithConstructorArgument("connectionStringName", "Tour");
+            Bind<ApplicationDbContext>().ToSelf().WithConstructorArgument("connectionStringName", "Tour");
         }
     }
 }
