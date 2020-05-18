f31df9797dc67a8a5bc79e55bb752ae18c379d6f
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-05-18 14:22:54 +0300

    Add models for accounts and roles

---
+++ Coursework/WebAPI/Models/UserReturnModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPI.Models
+{
+    public class UserReturnModel
+    {
+        public string Url { get; set; }
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public IList<string> Roles { get; set; }
+        public IList<System.Security.Claims.Claim> Claims { get; set; }
+    }
+}
