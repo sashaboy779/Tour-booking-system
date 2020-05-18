fcf8d79b900c0b04cf94e97f14ac8d9dbf18aaf7
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-05-18 14:10:16 +0300

    Add OAuth provider and JWT creator

---
+++ Coursework/BLL/Providers/CustomOAuthProvider.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BLL.Providers
+{
+    public class CustomOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly UserManager<ApplicationUser> manager;
+
+        public CustomOAuthProvider(UserManager<ApplicationUser> userManager)
+        {
+            manager = userManager;
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            var allowedOrigin = "*";
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+            ApplicationUser user = await manager.FindAsync(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            ClaimsIdentity oAuthIdentity = await manager.CreateIdentityAsync(user, "JWT");
+            var ticket = new AuthenticationTicket(oAuthIdentity, null);
+            context.Validated(ticket);
+        }
+    }
+}
