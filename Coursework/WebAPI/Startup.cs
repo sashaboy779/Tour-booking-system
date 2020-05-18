ddf2609a82b6dbc66da841676bcd1b6d2a57541c
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-05-18 14:21:17 +0300

    Configure Web API to use Ninject as dependency resolver

---
+++ Coursework/WebAPI/Startup.cs
@@ -0,0 +1,82 @@
+﻿using BLL.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Ninject;
+using System.Reflection;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfig = new HttpConfiguration();
+            ConfigureOAuthTokenGeneration(app);
+            ConfigureOAuthTokenConsumption(app); 
+            ConfigureWebApi(httpConfig);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseNinject(CreateKernel);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.Load("DependencyResolution"));
+            return kernel;
+        }
+
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            // Plugin the OAuth bearer JSON Web Token tokens generation and Consumption will be here
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = CreateKernel().Get<CustomOAuthProvider>(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:44314")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+
+        private void ConfigureOAuthTokenConsumption(IAppBuilder app)
+        {
+            var issuer = "http://localhost:44314";
+            string audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            byte[] audienceSecret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["as:AudienceSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audienceId },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, audienceSecret)
+                    }
+                });
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
