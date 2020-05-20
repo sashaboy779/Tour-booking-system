cf083cdc20009a2cd1184e655774257d50e7aa89
Vladyslav-Serko <Vladyslav.Serko@livenau.net>
2020-05-20 15:55:07 +0300

    Added nullable properties in Travel

--- Coursework/DAL/Entity/Travel.cs
+++ Coursework/DAL/Entity/Travel.cs
@@ -6,10 +6,10 @@ namespace DAL.Entity
     {
         public int Id { get; set; }
         public bool IsIncluded { get; set; }
-        public DateTime Departure { get; set; }
+        public DateTime? Departure { get; set; }
         public DateTime Arrival { get; set; }
         public string DepartureCity { get; set; }
         public string ArrivalCity { get; set; }
-        public TransportType TransportType { get; set; }
+        public TransportType? TransportType { get; set; }
     }
 }
---
+++ Coursework/DAL/Migrations/202005201252436_AddedNullableFieldsInTravel.cs
@@ -0,0 +1,20 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNullableFieldsInTravel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Travels", "Departure", c => c.DateTime());
+            AlterColumn("dbo.Travels", "TransportType", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Travels", "TransportType", c => c.Int(nullable: false));
+            AlterColumn("dbo.Travels", "Departure", c => c.DateTime(nullable: false));
+        }
+    }
+}
