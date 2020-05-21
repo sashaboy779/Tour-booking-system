191652ddb270dc7925e5c23d31c5c66e2d8bebc6
Artem Shiyan <artemon17021@gmail.com>
2020-05-21 20:13:58 +0300

    Add QueryParameters

---
+++ Coursework/BLL/Dto/QueryParameters/TourVariantParameters.cs
@@ -0,0 +1,17 @@
+﻿using BLL.Dto.Enums;
+
+namespace BLL.Dto.QueryParameters
+{
+    public class TourVariantParameters
+    {
+        public int TourId { get; set; }
+        public int MinTourists { get; set; } 
+        public int MaxTourists{ get; set; }
+        public double MinPersonPrice { get; set; }
+        public double MaxPersonPrice { get; set; }
+        public RoomType? Room { get; set; }
+        public Food? Food { get; set; }
+        public TransportType? Transport { get; set; }            
+        public bool TravelIncluded { get; set; }
+    }
+}
