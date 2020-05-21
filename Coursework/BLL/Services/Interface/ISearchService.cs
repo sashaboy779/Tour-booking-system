4d3ed617e6fde7414dc639d83dff70d56a49a6e1
Artem Shiyan <artemon17021@gmail.com>
2020-05-21 20:14:29 +0300

    Add SearchService

---
+++ Coursework/BLL/Services/Interface/ISearchService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BLL.Dto.QueryParameters;
+using BLL.Dto.Responses;
+
+namespace BLL.Services.Interface
+{
+    public interface ISearchService
+    {
+        ResortDto GetResortById(int id);
+        List<ResortDto> GetResorts(ResortParameters parameters);
+        List<TourDto> GetTours(TourParameters parameter);
+        TourDto GetTourById(int id);
+        List<TourDto> GetToursByRatingMore(double rating);
+        List<TourVariantDto> GetTourVariants(TourVariantParameters parameters);
+    }
+}
