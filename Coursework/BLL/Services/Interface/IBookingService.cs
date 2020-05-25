4fb00fa761bb49152487b83c1929cc5262e10a4a
Vladyslav-Serko <Vladyslav.Serko@livenau.net>
2020-05-25 23:10:13 +0300

    Added tours booking

---
+++ Coursework/BLL/Services/Interface/IBookingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Dto.TourBooking;
+
+namespace BLL.Services.Interface
+{
+    public interface IBookingService
+    {
+        void BookTour(Booking booking);
+        void CancelTourBooking(Booking booking);   
+    }
+}
